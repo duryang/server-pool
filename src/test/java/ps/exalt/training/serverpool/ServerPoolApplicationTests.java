ab476f425d6a152fbecaa774a41da34ad3c88a26
Gor Duryan <unduryan@gmail.com>
2021-02-10 18:46:45 +0400

    Initial commit.

---
+++ src/test/java/ps/exalt/training/serverpool/ServerPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ps.exalt.training.serverpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
